d1b58a570cf153eed831b76bd77db2020f83d09c
MarcoVela <mvelar@uni.pe>
2020-06-26 16:21:12 -0500

    PMV

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
